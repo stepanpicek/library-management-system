3b8df20aac87c8df6bec8003750c3c4d89c406f6
Štěpán Picek <stepan.picek@designeo.cz>
2026-02-08 15:25:29 +0100

    Fix nullables

--- src/LibraryManagementSystem.Application/Books/Queries/GetBookDetail/BookDetailDto.cs
+++ src/LibraryManagementSystem.Application/Books/Queries/GetBookDetail/BookDetailDto.cs
@@ -3,10 +3,10 @@ namespace LibraryManagementSystem.Application.Books.Queries.GetBookDetail;
 public class BookDetailDto
 {
     public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
+    public required string Title { get; set; }
+    public required string Author { get; set; }
     public int Year { get; set; }
-    public string Isbn { get; set; }
+    public required string Isbn { get; set; }
     public int AvailableCopies { get; set; }
-    public IReadOnlyCollection<LoanDto> Loans { get; set; }
+    public IReadOnlyCollection<LoanDto> Loans { get; set; } = [];
 }
--- src/LibraryManagementSystem.Application/Books/Queries/GetBooks/BookDto.cs
+++ src/LibraryManagementSystem.Application/Books/Queries/GetBooks/BookDto.cs
@@ -3,9 +3,9 @@ namespace LibraryManagementSystem.Application.Books.Queries.GetBooks;
 public class BookDto
 {
     public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
+    public required string Title { get; set; }
+    public required string Author { get; set; }
     public int Year { get; set; }
-    public string Isbn { get; set; }
+    public required string Isbn { get; set; }
     public int AvailableCopies { get; set; }
 }
